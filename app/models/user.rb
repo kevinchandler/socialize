5a83920f6a12648a1d843592b60dce0b8ea5e5e0
Kevin Chandler <im.kevin@me.com>
2016-06-20 21:19:35 -0700

    add posts association

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,7 @@ class User < ActiveRecord::Base
   validates_uniqueness_of :username, scope: :source
   validates_presence_of [:username, :source, :first_encountered]
 
+  has_many :posts
+
   scope :reddit, -> { where(source: 'reddit') }
 end
