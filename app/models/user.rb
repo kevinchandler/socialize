bf0e4349d689dfe5fb2c84556e6b565f4b07d7a8
Kevin Chandler <im.kevin@me.com>
2016-06-29 23:16:33 -0700

    set first_encountered if not present

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,18 @@
 class User < ActiveRecord::Base
   validates_uniqueness_of :username, scope: :source
-  validates_presence_of [:username, :source, :first_encountered]
+  validates_presence_of [:username, :source]
+
+  after_create :check_first_encountered
 
   has_many :posts
   belongs_to :source
 
   scope :reddit, -> { where(source: Source.reddit) }
   scope :twitter, -> { where(source: Source.twitter) }
+
+  def check_first_encountered
+    return unless first_encountered.nil?
+    first_encountered = Date.today
+    save
+  end
 end
