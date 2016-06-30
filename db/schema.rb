42534ddb89adc2e89e5919cee40b1e3c02e637da
Kevin Chandler <im.kevin@me.com>
2016-06-29 22:38:52 -0700

    add created_at to posts

--- db/schema.rb
+++ db/schema.rb
@@ -11,19 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160621041952) do
+ActiveRecord::Schema.define(version: 20160630053753) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "posts", force: :cascade do |t|
-    t.integer "user_id"
-    t.integer "source_id"
-    t.text    "title"
-    t.text    "body"
-    t.date    "date"
-    t.string  "identifier"
-    t.text    "meta_info"
+    t.integer  "user_id"
+    t.integer  "source_id"
+    t.text     "title"
+    t.text     "body"
+    t.date     "date"
+    t.string   "identifier"
+    t.text     "meta_info"
+    t.datetime "created_at"
   end
 
   add_index "posts", ["identifier"], name: "index_posts_on_identifier", using: :btree
